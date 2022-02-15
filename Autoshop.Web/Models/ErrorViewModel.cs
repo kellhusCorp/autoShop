a9fc1dbf0777d128917bfcfd1f04abf3109229cc
kellhusCorp <alkan_7@mail.ru>
2022-02-15 19:16:57 +0400

    init commit

---
+++ Autoshop.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Autoshop.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
