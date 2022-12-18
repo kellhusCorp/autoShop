66e40cc187cc8698e29290ac3e6aca4af01bea73
kellhusCorp <alkan_7@mail.ru>
2022-12-18 14:37:01 +0400

    3. Fixed seed data method

--- Autoshop.Services.ProductAPI/Migrations/20220315160600_SeedProducts.cs
+++ Autoshop.Services.ProductAPI/Migrations/20220315160600_SeedProducts.cs
@@ -13,10 +13,10 @@ namespace Autoshop.Services.ProductAPI.Migrations
                 columns: new[] { "ProductId", "CategoryName", "Description", "ImageUrl", "Name", "Price" },
                 values: new object[,]
                 {
-                    { 1, "Appetizer", "Praesent scelerisque, mi sed", "", "Samosa", 15.0 },
-                    { 2, "Appetizer", "Praesent scelerisque, mi sed", "", "Samosa", 13.99 },
-                    { 3, "Dessert", "Praesent scelerisque, mi sed", "", "Samosa", 22.0 },
-                    { 4, "Entree", "Praesent scelerisque, mi sed", "", "Samosa", 15.0 }
+                    { 1, "Main battle tanks", "", "https://upload.wikimedia.org/wikipedia/commons/thumb/2/24/Leopard_2_A5_der_Bundeswehr.jpg/300px-Leopard_2_A5_der_Bundeswehr.jpg", "Leopard 2", 150000.0 },
+                    { 2, "Main battle tanks", "", "https://upload.wikimedia.org/wikipedia/commons/thumb/2/27/ZTZ-99A_MBT_20170716.jpg/300px-ZTZ-99A_MBT_20170716.jpg", "Type 99A", 200000.0 },
+                    { 3, "Main battle tanks", "", "https://upload.wikimedia.org/wikipedia/commons/thumb/3/30/Challenger_2_Main_Battle_Tank_patrolling_outside_Basra%2C_Iraq_MOD_45148325.jpg/300px-Challenger_2_Main_Battle_Tank_patrolling_outside_Basra%2C_Iraq_MOD_45148325.jpg", "Challenger 2", 100000.0 },
+                    { 4, "Main battle tanks", "", "https://upload.wikimedia.org/wikipedia/commons/thumb/9/92/Mounted_Soldier_System_%28MSS%29.jpg/300px-Mounted_Soldier_System_%28MSS%29.jpg", "M1A2", 175000.0 }
                 });
         }
 
