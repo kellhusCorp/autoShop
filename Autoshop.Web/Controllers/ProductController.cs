7dfb9f70257b335f15f3d1403405c9fcf994bdd5
kellhusCorp <alkan_7@mail.ru>
2022-12-18 14:38:32 +0400

    5. Added edit & delete methods in ProductController.

--- Autoshop.Web/Controllers/ProductController.cs
+++ Autoshop.Web/Controllers/ProductController.cs
@@ -46,4 +46,63 @@ public class ProductController : Controller
         
         return View(model);
     }
+
+    [HttpGet]
+    [Route("Product/Edit/{productId:int}")]
+    public async Task<IActionResult> Edit(int productId)
+    {
+        var response = await _productService.GetProductByIdAsync<ResponseDto>(productId);
+        if (response != null && response.IsSuccess)
+        {
+            var productDto = JsonConvert.DeserializeObject<ProductDto>(Convert.ToString(response.Result));
+            return View(productDto);
+        }
+        return NotFound();
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> Delete(int productId)
+    {
+        var response = await _productService.GetProductByIdAsync<ResponseDto>(productId);
+        if (response != null && response.IsSuccess)
+        {
+            var model = JsonConvert.DeserializeObject<ProductDto>(response.Result.ToString());
+            return View(model);
+        }
+
+        return NotFound();
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Delete(ProductDto model)
+    {
+        if (model.ProductId != 0)
+        {
+            var response = await _productService.DeleteProductAsync<ResponseDto>(model.ProductId);
+            if (response.IsSuccess)
+            {
+                return RedirectToAction(nameof(Index));
+            }
+        }
+
+        return View(model);
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Edit(ProductDto model)
+    {
+        if (ModelState.IsValid)
+        {
+            var response = await _productService.UpdateProductAsync<ResponseDto>(model);
+            if (response != null && response.IsSuccess)
+            {
+                return RedirectToAction(nameof(Index));
+            }
+        }
+        
+        return View(model);
+    }
+    
 }
